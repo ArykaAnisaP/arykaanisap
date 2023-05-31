a50e3a1bb18a15a363918dd63dceaa074d854616
ArykaAnisaP <arykaanisap22@gmail.com>
2023-05-31 14:10:37 +0700

    PERT11

--- main.go
+++ main.go
@@ -8,12 +8,27 @@ import (
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
+
 	"github.com/whatsauth/whatsauth"
 
 	"github.com/ArykaAnisaP/arykaanisap/url"
 
 	"github.com/gofiber/fiber/v2"
+
+	_ "github.com/ArykaAnisaP/arykaanisap/docs"
 )
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+
 
 func main() {
 	go whatsauth.RunHub()
