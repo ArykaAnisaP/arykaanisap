6e3f86241f6bf633739bc6ece47b96a72b06b966
ArykaAnisaP <arykaanisap22@gmail.com>
2023-05-31 14:55:47 +0700

    pe11

--- controller/coba.go
+++ controller/coba.go
@@ -78,6 +78,14 @@ func GetAllGajiFromPresensi(c *fiber.Ctx) error {
 	return c.JSON(ps)
 }
 
+// GetAllPresensi godoc
+// @Summary Get All Data Presensi.
+// @Description Mengambil semua data presensi.
+// @Tags Presensi
+// @Accept json
+// @Produce json
+// @Success 200 {object} Presensi
+// @Router /presensi [get]
 func GetAllPresensi(c *fiber.Ctx) error {
 	ps := inimodulee.GetAllPresensi(config.Ulbimongoconn, "presensi")
 	return c.JSON(ps)
